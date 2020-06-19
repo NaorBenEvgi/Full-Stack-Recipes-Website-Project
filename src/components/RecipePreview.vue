c10650082f1ceb913c2cf48f54d20db04aeb5f12
NaorBenEvgi <51819075+NaorBenEvgi@users.noreply.github.com>
2020-06-19 16:06:57 +0300

    Fit templete to our server + cookies works (#1)
    * random+fullRecipe+login
    
    * register works!
    
    * cookies works

--- src/components/RecipePreview.vue
+++ src/components/RecipePreview.vue
@@ -4,7 +4,7 @@
     class="recipe-preview"
   >
     <div class="recipe-body">
-      <img v-if="image_load" :src="recipe.image" class="recipe-image" />
+      <img :src="recipe.image" class="recipe-image" />
     </div>
     <div class="recipe-footer">
       <div :title="recipe.title" class="recipe-title">
@@ -12,7 +12,7 @@
       </div>
       <ul class="recipe-overview">
         <li>{{ recipe.readyInMinutes }} minutes</li>
-        <li>{{ recipe.aggregateLikes }} likes</li>
+        <li>{{ recipe.popularity }} likes</li>
       </ul>
     </div>
   </router-link>
@@ -20,14 +20,14 @@
 
 <script>
 export default {
-  mounted() {
-    this.axios.get(this.recipe.image).then((i) => {
-      this.image_load = true;
-    });
-  },
+  // mounted() {
+  //   this.axios.get(this.recipe.image).then((i) => {
+  //     this.image_load = true;
+  //   });
+  // },
   data() {
     return {
-      image_load: false
+      // image_load: false
     };
   },
   props: {
