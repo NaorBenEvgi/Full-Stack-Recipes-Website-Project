16eb2da3f302b1ff89b782183ef3473b31905f1c
NaorBenEvgi <naorbenevgi@gmail.com>
2020-06-21 14:41:39 +0300

    About page is ready

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,6 +1,41 @@
 <template>
   <div class="container">
-    <h1 class="title">About Page</h1>
+    <h1 id="logo" class="title">About Us</h1>
+    <div>
+      <b-card
+        title="Naor Ben Ivgi & Roy Judes"
+        img-src="https://res.cloudinary.com/dmoaoiupe/image/upload/v1592737577/recipes/IMG-9796_hbw6ac.jpg"
+        img-alt="Card image"
+        img-top
+        sub-title="B.Sc Third Year Software and Information System Engineering Student in Ben Gurion University of the Negev"
+        img-width="100"
+        img-height="600"
+      >
+        <b-card-text>
+          Some quick example text to build on the
+          <em>card title</em> and make up the bulk of the card's
+          content.
+        </b-card-text>
+
+        <b-card-text>
+          <strong>Contact us: naorbe@post.bgu.ac.il / royju@post.bgu.ac.il.</strong>
+        </b-card-text>
+
+        <div>
+          Our previous assignments:
+          <a
+            href="https://sise-web-development-environments.github.io/Pacman-Roy-Naor/"
+            class="card-link"
+          >PacMan Game (Ass 2)</a>
+
+          <a
+            href="https://sise-web-development-environments.github.io/assignment1-HTML-website/"
+            class="card-link"
+          >Personal Page - Naor (Ass 1)</a>
+        </div>
+
+      </b-card>
+    </div>
   </div>
 </template>
 
@@ -9,4 +44,15 @@ export default {};
 </script>
 
 <style>
+@import url(
+  https://fonts.googleapis.com/css?family=Open+Sans:400,
+  800,
+  700,
+  300
+);
+@import url(https://fonts.googleapis.com/css?family=Squada+One);
+body {
+  background: #eee
+    url(https://subtlepatterns.com/patterns/extra_clean_paper.png);
+}
 </style>
