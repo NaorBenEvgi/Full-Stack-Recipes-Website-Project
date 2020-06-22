04e97ea644736dfcde12cd9598cfbd45cc69ac29
NaorBenEvgi <51819075+NaorBenEvgi@users.noreply.github.com>
2020-06-22 16:25:51 +0300

    watch and fav indications work + heart is filled when you click and add it to the fav list (#7)

--- src/main.js
+++ src/main.js
@@ -28,7 +28,9 @@ import {
   ToastPlugin,
   LayoutPlugin,
   FormCheckboxPlugin,
-  FormRadioPlugin	
+  FormRadioPlugin,
+  BootstrapVue,
+  BootstrapVueIcons
 } from "bootstrap-vue";
 [
   FormGroupPlugin,
@@ -42,16 +44,18 @@ import {
   ToastPlugin,
   LayoutPlugin,
   FormCheckboxPlugin,
-  FormRadioPlugin	
+  FormRadioPlugin,
+  BootstrapVue,
+  BootstrapVueIcons
 ].forEach((x) => Vue.use(x));
 Vue.use(Vuelidate);
 
 axios.interceptors.request.use(
-  function(config) {
+  function (config) {
     // Do something before request is sent
     return config;
   },
-  function(error) {
+  function (error) {
     // Do something with request error
     return Promise.reject(error);
   }
@@ -59,11 +63,11 @@ axios.interceptors.request.use(
 
 // Add a response interceptor
 axios.interceptors.response.use(
-  function(response) {
+  function (response) {
     // Do something with response data
     return response;
   },
-  function(error) {
+  function (error) {
     // Do something with response error
     return Promise.reject(error);
   }
